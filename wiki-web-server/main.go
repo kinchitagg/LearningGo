d41cce24cdcb0982b867913a09361da62df910f6
Kinchit Aggarwal <Kinchit.Aggarwal@acefone.com>
2023-08-02 10:02:33 +0530

    wikki project updated with edit and save feature

--- wiki-web-server/main.go
+++ wiki-web-server/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 type Page struct {
 	Title string
@@ -27,35 +31,73 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: string(body)}, nil
 }
 
-func userInput() (string, string) {
-	var title string
-	bodyReader := bufio.NewReader(os.Stdin)
-	fmt.Println("Welcome to your wiki")
-	fmt.Println("Kindly enter your document title")
-	fmt.Scanln(&title)
-	fmt.Println("Enter your body")
-	body, _ := bodyReader.ReadString('\n')
-	return title, body
-}
+// func userInput() (string, string) {
+// 	var title string
+// 	bodyReader := bufio.NewReader(os.Stdin)
+// 	fmt.Println("Welcome to your wiki")
+// 	fmt.Println("Kindly enter your document title")
+// 	fmt.Scanln(&title)
+// 	fmt.Println("Enter your body")
+// 	body, _ := bodyReader.ReadString('\n')
+// 	return title, body
+// }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/"):]
-	p, _ := loadPage(title)
-	fmt.Println(p.Title, p.Body)
-	fmt.Fprintf(w, "<h1>%s<h1><p>%s<p>", p.Title, p.Body)
-
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Println("error found")
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		//http.Redirect()
+	}
+	renderTemplate(w, "view", p)
 }
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: body}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, templ string, p *Page) {
+	err := templates.ExecuteTemplate(w, templ+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+
+// }
 func main() {
 
-	title, body := userInput()
-	pIn := &Page{
-		Title: title,
-		Body:  body,
-	}
-	pIn.save()
+	//title, body := userInput()
+	// pIn := &Page{
+	// 	Title: title,
+	// 	Body:  body,
+	// }
+	//pIn.save()
 	//pOut, _ := loadPage(pIn.Title)
 
-	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
